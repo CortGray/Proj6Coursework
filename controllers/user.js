e9f20d5a090b448a66db2b6b1830e7baee347e6e
Cort Gray <cortkatgray@gmail.com>
2022-06-30 07:16:20 -0700

    what did I change??

--- controllers/user.js
+++ controllers/user.js
@@ -47,7 +47,7 @@ exports.login = (req, res, next) => {
                 res.status(500).json({
                     error: error
                 });
-            };
+            })
         .catch((error) => {
             res.status(500).json({
                 error: error
