ddf5628d9a02bd54d9e84c229207384e648bd937
Cort Gray <cortkatgray@gmail.com>
2022-07-03 19:52:04 -0700

    plugged security issue in Delete route

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -60,18 +60,33 @@ exports.modifyThing = (req, res, next) => {
 }
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({
+    Thing.findOne({
         _id: req.params.id
     })
-        .then(() => {
-            req.status(200).json({
-                message: 'Deleted!'
-            });
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error
-            });
+        .then((thing) => {
+            if (!thing) {
+                return res.status(404).json({
+                    error: new Error('No such thing!')
+                });
+            }
+            if (thing.userId !== req.auth.userId) {
+                return res.status(400).json({
+                    error: new Error('Unauthorized request.')
+                });
+            }
+            Thing.deleteOne({
+                _id: req.params.id
+            })
+                .then(() => {
+                    req.status(200).json({
+                        message: 'Deleted!'
+                    });
+                })
+                .catch((error) => {
+                    res.status(400).json({
+                        error: error
+                    });
+                });
         });
 }
 
