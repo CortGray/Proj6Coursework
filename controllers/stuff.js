ba120dfa7e47242946a999a107c295d7ac32da73
Cort Gray <cortkatgray@gmail.com>
2022-07-04 15:08:52 -0700

    modified delete function to include files

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
@@ -80,27 +81,19 @@ exports.deleteThing = (req, res, next) => {
         _id: req.params.id
     })
         .then((thing) => {
-            if (!thing) {
-                return res.status(404).json({
-                    error: new Error('No such thing!')
-                });
-            }
-            if (thing.userId !== req.auth.userId) {
-                return res.status(400).json({
-                    error: new Error('Unauthorized request.')
-                });
-            }
-            Thing.deleteOne({
-                _id: req.params.id
-            })
-                .then(() => {
-                    req.status(200).json({
-                        message: 'Deleted!'
-                    });
+            const filename = thing.imageUrl.split('/images/')[1];
+            fs.unlink('images/' + filename, () => {
+                Thing.deleteOne({
+                    _id: req.params.id
                 })
-                .catch((error) => {
-                    res.status(400).json({
-                        error: error
+                    .then(() => {
+                        req.status(200).json({
+                            message: 'Deleted!'
+                        });
+                    }) .catch((error) => {
+                        res.status(400).json({
+                            error: error
+                        });
                     });
                 });
         });
